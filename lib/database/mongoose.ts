a4f4f6f4e6b1ee70785a91d9ba95a82317c9f139
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-04-15 15:11:42 +0300

    MongoDB

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,37 @@
+// connection to MongoDB
+import mongoose, { Mongoose, mongo } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+// in Next js you connect to the DB in every request or server action, because Next js runs in the serverless environment. Each request is handled independently
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "imaginify",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
